62c9e8d1c381ae120befe42a76b3c404e62a248b
Ieriz Nicolle <ieriznicolle@gmail.com>
2020-03-02 20:09:41 +0800

    Wireframe
    File check, Generate Template, & Wireframe

---
+++ Auto Encoding of Grades in SIS/AEGIS.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class AEGS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AEGS))
+        Me.TextBoxUN = New System.Windows.Forms.TextBox()
+        Me.LabelUN = New System.Windows.Forms.Label()
+        Me.LabelPW = New System.Windows.Forms.Label()
+        Me.TextBoxPW = New System.Windows.Forms.TextBox()
+        Me.LabelGS = New System.Windows.Forms.Label()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.DialogBrowse = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBoxGS = New System.Windows.Forms.TextBox()
+        Me.BtnEncode = New System.Windows.Forms.Button()
+        Me.WebBrowser = New System.Windows.Forms.WebBrowser()
+        Me.GetGSTemplate = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBoxUN
+        '
+        Me.TextBoxUN.BackColor = System.Drawing.Color.Maroon
+        Me.TextBoxUN.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBoxUN.Font = New System.Drawing.Font("Montserrat", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxUN.Location = New System.Drawing.Point(12, 155)
+        Me.TextBoxUN.Name = "TextBoxUN"
+        Me.TextBoxUN.Size = New System.Drawing.Size(235, 24)
+        Me.TextBoxUN.TabIndex = 0
+        '
+        'LabelUN
+        '
+        Me.LabelUN.AutoSize = True
+        Me.LabelUN.BackColor = System.Drawing.Color.Transparent
+        Me.LabelUN.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LabelUN.Font = New System.Drawing.Font("Montserrat Light", 9.7!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelUN.ForeColor = System.Drawing.Color.Gainsboro
+        Me.LabelUN.Location = New System.Drawing.Point(12, 132)
+        Me.LabelUN.Name = "LabelUN"
+        Me.LabelUN.Size = New System.Drawing.Size(82, 20)
+        Me.LabelUN.TabIndex = 1
+        Me.LabelUN.Text = "User Name"
+        '
+        'LabelPW
+        '
+        Me.LabelPW.AutoSize = True
+        Me.LabelPW.BackColor = System.Drawing.Color.Transparent
+        Me.LabelPW.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LabelPW.Font = New System.Drawing.Font("Montserrat Light", 9.7!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelPW.ForeColor = System.Drawing.Color.Gainsboro
+        Me.LabelPW.Location = New System.Drawing.Point(12, 206)
+        Me.LabelPW.Name = "LabelPW"
+        Me.LabelPW.Size = New System.Drawing.Size(71, 20)
+        Me.LabelPW.TabIndex = 3
+        Me.LabelPW.Text = "Password"
+        '
+        'TextBoxPW
+        '
+        Me.TextBoxPW.BackColor = System.Drawing.Color.Maroon
+        Me.TextBoxPW.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBoxPW.Font = New System.Drawing.Font("Montserrat", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxPW.Location = New System.Drawing.Point(12, 229)
+        Me.TextBoxPW.Name = "TextBoxPW"
+        Me.TextBoxPW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.TextBoxPW.Size = New System.Drawing.Size(235, 23)
+        Me.TextBoxPW.TabIndex = 2
+        '
+        'LabelGS
+        '
+        Me.LabelGS.AutoSize = True
+        Me.LabelGS.BackColor = System.Drawing.Color.Transparent
+        Me.LabelGS.Font = New System.Drawing.Font("Montserrat Light", 9.7!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelGS.ForeColor = System.Drawing.Color.LightGray
+        Me.LabelGS.Location = New System.Drawing.Point(14, 286)
+        Me.LabelGS.Name = "LabelGS"
+        Me.LabelGS.Size = New System.Drawing.Size(87, 20)
+        Me.LabelGS.TabIndex = 4
+        Me.LabelGS.Text = "GradeSheet"
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.BackColor = System.Drawing.Color.Maroon
+        Me.BtnBrowse.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(101, Byte), Integer), CType(CType(8, Byte), Integer), CType(CType(11, Byte), Integer))
+        Me.BtnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnBrowse.Font = New System.Drawing.Font("Montserrat Light", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBrowse.ForeColor = System.Drawing.Color.Gainsboro
+        Me.BtnBrowse.Location = New System.Drawing.Point(29, 338)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(85, 31)
+        Me.BtnBrowse.TabIndex = 5
+        Me.BtnBrowse.Text = "Browse"
+        Me.BtnBrowse.UseVisualStyleBackColor = False
+        '
+        'DialogBrowse
+        '
+        Me.DialogBrowse.InitialDirectory = "C:\"
+        Me.DialogBrowse.Title = "Select Grade Sheet"
+        '
+        'TextBoxGS
+        '
+        Me.TextBoxGS.BackColor = System.Drawing.Color.Maroon
+        Me.TextBoxGS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBoxGS.Font = New System.Drawing.Font("Montserrat", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxGS.Location = New System.Drawing.Point(12, 309)
+        Me.TextBoxGS.Name = "TextBoxGS"
+        Me.TextBoxGS.Size = New System.Drawing.Size(235, 23)
+        Me.TextBoxGS.TabIndex = 6
+        '
+        'BtnEncode
+        '
+        Me.BtnEncode.BackColor = System.Drawing.Color.Maroon
+        Me.BtnEncode.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(101, Byte), Integer), CType(CType(8, Byte), Integer), CType(CType(11, Byte), Integer))
+        Me.BtnEncode.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnEncode.Font = New System.Drawing.Font("Montserrat Light", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEncode.ForeColor = System.Drawing.Color.Gainsboro
+        Me.BtnEncode.Location = New System.Drawing.Point(140, 338)
+        Me.BtnEncode.Name = "BtnEncode"
+        Me.BtnEncode.Size = New System.Drawing.Size(91, 31)
+        Me.BtnEncode.TabIndex = 7
+        Me.BtnEncode.Text = "Encode"
+        Me.BtnEncode.UseVisualStyleBackColor = False
+        '
+        'WebBrowser
+        '
+        Me.WebBrowser.Location = New System.Drawing.Point(259, 0)
+        Me.WebBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser.Name = "WebBrowser"
+        Me.WebBrowser.Size = New System.Drawing.Size(725, 558)
+        Me.WebBrowser.TabIndex = 8
+        '
+        'GetGSTemplate
+        '
+        Me.GetGSTemplate.FlatAppearance.BorderSize = 0
+        Me.GetGSTemplate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GetGSTemplate.Font = New System.Drawing.Font("Montserrat", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GetGSTemplate.ForeColor = System.Drawing.Color.FromArgb(CType(CType(76, Byte), Integer), CType(CType(4, Byte), Integer), CType(CType(7, Byte), Integer))
+        Me.GetGSTemplate.Location = New System.Drawing.Point(29, 521)
+        Me.GetGSTemplate.Name = "GetGSTemplate"
+        Me.GetGSTemplate.Size = New System.Drawing.Size(202, 28)
+        Me.GetGSTemplate.TabIndex = 9
+        Me.GetGSTemplate.Text = "Get Gradesheet Template"
+        Me.GetGSTemplate.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DarkRed
+        Me.Panel1.Controls.Add(Me.BtnBrowse)
+        Me.Panel1.Controls.Add(Me.BtnEncode)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.TextBoxGS)
+        Me.Panel1.Controls.Add(Me.GetGSTemplate)
+        Me.Panel1.Controls.Add(Me.LabelUN)
+        Me.Panel1.Controls.Add(Me.LabelGS)
+        Me.Panel1.Controls.Add(Me.LabelPW)
+        Me.Panel1.Controls.Add(Me.TextBoxPW)
+        Me.Panel1.Controls.Add(Me.TextBoxUN)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(261, 561)
+        Me.Panel1.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Montserrat Black", 50.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(260, 104)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "AEGiS"
+        '
+        'AEGS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(984, 561)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.WebBrowser)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "AEGS"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto Encoding of Grades in SIS"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextBoxUN As TextBox
+    Friend WithEvents LabelUN As Label
+    Friend WithEvents LabelPW As Label
+    Friend WithEvents TextBoxPW As TextBox
+    Friend WithEvents LabelGS As Label
+    Friend WithEvents BtnBrowse As Button
+    Friend WithEvents DialogBrowse As OpenFileDialog
+    Friend WithEvents TextBoxGS As TextBox
+    Friend WithEvents BtnEncode As Button
+    Friend WithEvents WebBrowser As WebBrowser
+    Friend WithEvents GetGSTemplate As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Auto Encoding of Grades in SIS/AEGIS.vb
@@ -0,0 +1,198 @@
+﻿
+Imports Excel = Microsoft.Office.Interop.Excel
+Public Class AEGS
+    Dim FileNameGS As String            'FILE NAME - GRADE SHEET EXCEL FILE
+
+    Private Sub AEGS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBoxUN_TextChanged(sender As Object, e As EventArgs) Handles TextBoxUN.TextChanged
+
+    End Sub
+
+    Private Sub LabelGS_Click(sender As Object, e As EventArgs) Handles LabelPW.Click
+
+    End Sub
+
+    Private Sub PW_TextChanged_1(sender As Object, e As EventArgs) Handles TextBoxPW.TextChanged
+
+    End Sub
+
+    Private Sub BtnBrowse_Click(sender As Object, e As EventArgs) Handles BtnBrowse.Click
+        'BROWSE FOR GRADE SHEET EXCEL FILE
+        DialogBrowse.ShowDialog()
+        FileNameGS = DialogBrowse.FileName
+        TextBoxGS.AppendText(FileNameGS)
+    End Sub
+
+    Private Sub BtnEncode_Click(sender As Object, e As EventArgs) Handles BtnEncode.Click
+
+        '----------------------------GRADE SHEET VALIDATION--------------------------------------
+
+        'CHECK FILE EXIST
+        If System.IO.File.Exists(FileNameGS) Then
+            Dim xlApp As Excel.Application
+            Dim xlWb As Excel.Workbook
+            Dim xlWs As Excel.Worksheet
+            'Dim xlRan As Excel.Range
+
+            xlApp = New Excel.Application
+
+            If xlApp Is Nothing Then
+                MessageBox.Show("Excel is not properly installed!")
+                Return
+            End If
+
+            xlWb = xlApp.Workbooks.Open(FileNameGS)
+
+            xlApp.Visible = True
+            xlWs = xlWb.ActiveSheet
+
+            'CHECK INFO
+            Dim acadYr As String = xlWs.Range("B1").Value
+            Dim sem As String = xlWs.Range("B2").Value
+            Dim subjCode As String = xlWs.Range("B3").Value
+            Dim courseCode As String = xlWs.Range("B4").Value
+            Dim year As String = xlWs.Range("B5").Value
+            Dim section As String = xlWs.Range("B6").Value
+            Dim lastRow As Integer = xlWs.UsedRange.Rows.Count
+
+            If acadYr <> "" Then
+                If sem <> "" Then
+                    If subjCode <> "" Then
+                        If courseCode <> "" Then
+                            If year <> "" Then
+                                If section <> "" Then
+                                    MessageBox.Show("Completely filled out")
+                                    'WebBrowser.Navigate("https://pup.edu.ph")
+                                    Process.Start("https://www.twitter.com")
+
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+
+        Else
+            MessageBox.Show("The file " + FileNameGS + " does not exist.")
+            Return
+        End If
+
+    End Sub
+
+    Private Sub TextBoxGS_TextChanged(sender As Object, e As EventArgs) Handles TextBoxGS.TextChanged
+        FileNameGS = TextBoxGS.Text
+    End Sub
+
+    Private Sub GetGSTemplate_MouseEnter(sender As Object, e As EventArgs) Handles GetGSTemplate.MouseEnter
+        GetGSTemplate.ForeColor = Color.Tomato
+    End Sub
+
+    Private Sub GetGSTemplate_MouseLeave(sender As Object, e As EventArgs) Handles GetGSTemplate.MouseLeave
+        GetGSTemplate.ForeColor = Color.Firebrick
+    End Sub
+
+
+
+    Private Sub GetGSTemplate_Click(sender As Object, e As EventArgs) Handles GetGSTemplate.Click
+        'CREATE GRADE SHEET TEMPLATE IN EXCEL
+        Dim xlApp As Excel.Application
+        Dim xlWb As Excel.Workbook
+        Dim xlWs As Excel.Worksheet
+        Dim misValue As Object = System.Reflection.Missing.Value
+
+        'INITIALIZATION
+        xlApp = New Excel.Application
+
+        If xlApp Is Nothing Then
+            MessageBox.Show("Excel is not properly installed!")
+            Return
+        End If
+
+        xlWb = xlApp.Workbooks.Add(misValue)
+        xlWs = xlWb.Sheets("Sheet1")
+
+        'CONTENT
+        With xlWs
+
+            .Range("A1").Value = "School Year"
+            .Range("A2").Value = "Semester"
+            .Range("A3").Value = "Subject Code"
+            .Range("A4").Value = "Course Code"
+            .Range("A5").Value = "Year Level"
+            .Range("A6").Value = "Section"
+
+            .Range("B1").Value = "1920"
+            .Range("B2").Value = "First"
+            .Range("B3").Value = "COEN 3284"
+            .Range("B4").Value = "BSCOE"
+            .Range("B5").Value = "1"
+            .Range("B6").Value = "1"
+            .Range("B1:B6").HorizontalAlignment = Excel.Constants.xlLeft
+
+            .Columns(1).ColumnWidth = 12
+            .Range("A8").Value = "No"
+            .Range("A8:A9").Merge()
+
+            .Columns(2).ColumnWidth = 35
+            .Range("B8").Value = "Name"
+            .Range("B8:B9").Merge()
+
+            .Columns(3).ColumnWidth = 16
+            .Range("C8").Value = "Student Number"
+            .Range("C8:C9").Merge()
+
+            .Range("D8").Value = "Grade"
+            .Range("D8:E8").Merge()
+            .Columns(4).ColumnWidth = 8.5
+            .Range("D9").Value = "Midterm"
+            .Columns(5).ColumnWidth = 8.5
+            .Range("E9").Value = "Final"
+
+            .Columns(6).ColumnWidth = 10.5
+            .Range("F8").Value = "Final Grade"
+            .Range("F8:F9").Merge()
+
+            .Range("A10").Value = "1"
+            .Range("B10").Value = "DELA CRUZ, JUAN B."
+            .Range("C10").Value = "2020-00001-MN-0"
+            .Range("D10").Value = "1.00"
+            .Range("E10").Value = "1.00"
+            .Range("F10").Formula = "=AVERAGE(D10:E10)"
+
+            .Range("A8:F10").HorizontalAlignment = Excel.Constants.xlCenter
+            .Range("B10").HorizontalAlignment = Excel.Constants.xlLeft
+
+
+        End With
+        xlWb.SaveAs("C:\Users\" & SystemInformation.UserName & "\Documents\Grades Template.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
+          Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue)
+        xlWb.Close(True, misValue, misValue)
+        xlApp.Quit()
+
+        releaseObject(xlWs)
+        releaseObject(xlWb)
+        releaseObject(xlApp)
+
+        MessageBox.Show("Template saved, you can find the file in your documents folder.")
+
+
+    End Sub
+
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+
+    Private Sub WebBrowser_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser.DocumentCompleted
+
+    End Sub
+End Class
